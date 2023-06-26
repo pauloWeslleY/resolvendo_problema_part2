ec4c986b3ad418ee8524f7d7181e5e04fe88ed88
pauloWeslleY <weslleyspfc13@outlook.com>
2023-06-26 05:02:31 -0300

    feat: Finish

--- resolve_1.js
+++ resolve_1.js
@@ -1,33 +1,40 @@
 /**
- *  O problema de encontrar o número mais frequente em um array de números
- *  inteiros envolve percorrer o array e manter um registro da contagem de
- *  ocorrências de cada número.
- *  Ao percorrer o array, atualizamos um contador para cada número encontrado,
- *  mantendo o controle da frequência máxima e do número mais frequente
- *  encontrado até o momento.
+ *  @function findMostFrequentNumber() A função consiste em encontrar o número
+ *  mais frequente dentro do array de números inteiros, e envolve percorrer o array
+ *  e manter um registro da contagem de de cada número. Ao percorrer o array,
+ *  atualizar um counter para cada número encontrado no array, mantendo o
+ *  controle da frequência máxima e do número mais frequente encontrado.
  */
 
-function findMostFrequentNumber(array) {
+const findMostFrequentNumber = array => {
   let counter = {}
   let maxFrequency = 0
-  let numberMostFrequent = null
+  let numberMostFrequent
 
-  for (let number of array) {
-    if (counter[number]) {
-      counter[number]++
+  // Iteração para contagem dos números
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i]
+
+    // Faz a verificação incremento da contagem do número atual
+    if (counter[num]) {
+      counter[num]++
     } else {
-      counter[number] = 1
+      counter[num] = 1
     }
+  }
 
-    if (counter[number] > maxFrequency) {
-      maxFrequency = counter[number]
-      numberMostFrequent = number
+  // Iteração para encontrar o número mais frequente
+  for (const num in counter) {
+    if (counter[num] > maxFrequency) {
+      maxFrequency = counter[num]
+      numberMostFrequent = num
     }
   }
 
   return numberMostFrequent
 }
 
-let array = [1, 2, 3, 4, 5, 2, 2, 3, 3, 3]
-let numberMostFrequent = findMostFrequentNumber(array)
-console.log(numberMostFrequent)
+let arrNumber = [1, 3, 5, 3, 1, 1, 5, 1, 4]
+let numberMostFrequent = findMostFrequentNumber(arrNumber)
+
+console.log('Saída esperada: ', numberMostFrequent)
--- resolve_2.js
+++ resolve_2.js
@@ -1,14 +1,24 @@
-function findSumIndices(matrix, target) {
-  let map = {}
+/**
+ * @function findSumIndices() Nesta função eu começo com um objeto vazio chamado
+ * index, dentro dele ficara armazenados o números e seus indices logo em
+ * seguida faço um loop FOR para percorrer a matriz dos números a cada número
+ * atual eu calculo o complemento para conseguir obter o resultado da soma igual
+ * ao alvo e depois subtraio com o número atual, depois eu faço a verificação
+ * se existe o operador in dentro do objeto index, caso ele não exista armazeno
+ * o número atual.
+ */
+
+const findSumIndices = (matrix, target) => {
+  let index = {}
 
   for (let i = 0; i < matrix.length; i++) {
     let complement = target - matrix[i]
 
-    if (complement in map) {
-      return [map[complement], i]
+    if (complement in index) {
+      return [index[complement], i]
     }
 
-    map[matrix[i]] = i
+    index[matrix[i]] = i
   }
 
   return []
@@ -16,5 +26,6 @@ function findSumIndices(matrix, target) {
 
 let matrix = [2, 7, 11, 15]
 let target = 9
-let index = findSumIndices(matrix, target)
-console.log(index) // Output: [0, 1]
+let response = findSumIndices(matrix, target)
+
+console.log('Saída esperada: ', response)
